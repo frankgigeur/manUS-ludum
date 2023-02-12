9951372df1fc14f8401c290ffbcde71eea9f55eb
Charley <charlesouellet85@gmail.com>
2023-02-12 14:49:07 -0500

    Code de test pour les servomoteurs

---
+++ Manus_ludum/src/define.h
@@ -0,0 +1,9 @@
+
+#define HS422_CNT_SERVO_MIN 100     // Compteur minimum du PWM 12bit du PCA9865 pour le servomoteur HS422 (0°).
+#define HS422_CNT_SERVO_MID 307     // Compteur millieu du PWM 12bit du PCA9865 pour le servomoteur HS422 (90°).
+#define HS422_CNT_SERVO_MAX 525     // Compteur maximum du PWM 12bit du PCA9865 pour le servomoteur HS422 (180°).
+#define HS422_US_MIN  489           // L'équivalent en us du compteur minimum pour le PWM 12bit du PCA9865 pour le servomoteur HS422 (0°).
+#define HS422_US_MID  1506          // L'équivalent en us du compteur millieu pour le PWM 12bit du PCA9865 pour le servomoteur HS422 (90°).
+#define HS422_US_MAX  2563          // L'équivalent en us du compteur maximum pour le PWM 12bit du PCA9865 pour le servomoteur HS422 (180°).
+#define HS422_SERVO_FREQ 50         // Fréquence du PMW pour le servomoteur HS422.
+#define PCA9685_OSC_FREQ 25750000   // Fréquence d'oscillation du PCA9865 (Ajusté pour avoir une fréquence de 50Hz précisément).
---
+++ Manus_ludum/src/main.cpp
@@ -0,0 +1,29 @@
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+#include "define.h"
+
+// called this way, it uses the default address 0x40
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("8 channel Servo test!");
+
+  pwm.begin();
+  pwm.setOscillatorFrequency(PCA9685_OSC_FREQ);
+  pwm.setPWMFreq(HS422_SERVO_FREQ);
+ 
+  delay(10);
+}
+
+
+void loop() {
+  delay(2000);
+  pwm.setPWM(6, 0, HS422_CNT_SERVO_MAX);
+  pwm.setPWM(7, 0, HS422_CNT_SERVO_MAX);
+  delay(2000);
+  pwm.setPWM(6, 0, HS422_CNT_SERVO_MIN);
+  pwm.setPWM(7, 0, HS422_CNT_SERVO_MIN);
+}
